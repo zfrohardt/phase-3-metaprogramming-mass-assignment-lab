fd2fa4af1c5fdb7114cb1f4b36e3c8da26e88a09
Zach Frohardt <zfrohardt@gmail.com>
2021-05-20 17:08:14 -0700

    completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
   #your code here
+  def initialize(person)
+    person.each do |key, value| 
+      self.class.attr_accessor(key)
+      self.send(("#{key}="), value)
+    end
+  end
 end
